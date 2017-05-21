b8335677ee15fe58682e175f030d86ae35425fa1
Shamika La Shawn <Sg3147@utsnyc.edu>
2017-05-21 16:57:06 -0400

    Merge branch 'master' into frontEnd
    # Conflicts:
    #	CodingKatz/CodingKatz/__init__.pyc
    #	CodingKatz/CodingKatz/settings.pyc
    #	CodingKatz/CodingKatz/urls.pyc
    #	CodingKatz/CodingKatz/wsgi.pyc
    #	CodingKatz/apps/LoginAndRegistration/__init__.pyc
    #	CodingKatz/apps/LoginAndRegistration/admin.pyc
    #	CodingKatz/apps/LoginAndRegistration/migrations/__init__.pyc
    #	CodingKatz/apps/LoginAndRegistration/models.pyc
    #
    CodingKatz/apps/LoginAndRegistration/templates/LoginAndRegistration/inde
    x.html
    #
    CodingKatz/apps/LoginAndRegistration/templates/LoginAndRegistration/inde
    x2.html
    #	CodingKatz/apps/LoginAndRegistration/urls.pyc
    #	CodingKatz/apps/LoginAndRegistration/views.pyc
    #	CodingKatz/apps/User/__init__.pyc
    #	CodingKatz/apps/User/admin.pyc
    #	CodingKatz/apps/User/migrations/__init__.pyc
    #	CodingKatz/apps/User/models.pyc
    #	CodingKatz/apps/User/urls.pyc
    #	CodingKatz/apps/User/views.pyc
    #	CodingKatz/apps/Wall/__init__.pyc
    #	CodingKatz/apps/Wall/admin.pyc
    #	CodingKatz/apps/Wall/migrations/__init__.pyc
    #	CodingKatz/apps/Wall/models.pyc
    #	CodingKatz/apps/Wall/urls.pyc
    #	CodingKatz/apps/Wall/views.pyc
    #	CodingKatz/apps/__init__.pyc

--- CodingKatz/apps/LoginAndRegistration/urls.py
+++ CodingKatz/apps/LoginAndRegistration/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^1$', views.index1),
     url(r'^2$', views.index2),
     url(r'^login', views.login),
+    url(r'^first', views.first),
     url(r'^register', views.register),
 ]
